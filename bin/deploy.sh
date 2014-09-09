6ff3415cb6423a2bc15992b690962d09f8c9c590
Feross Aboukhadijeh <feross@feross.org>
2014-09-09 23:16:42 +0100

    update -> install

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,6 @@
 # Update code and restart server (run from app server)
 set -e
 cd /home/feross/www/instant.io && git pull
-cd /home/feross/www/instant.io && npm update --quiet
+cd /home/feross/www/instant.io && npm install --quiet
 cd /home/feross/www/instant.io && npm run build
 sudo supervisorctl reload && sleep 3 && sudo supervisorctl restart instant
