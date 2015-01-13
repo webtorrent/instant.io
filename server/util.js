52b606f35d70e5af280dba5d615f921d45e6a550
Feross Aboukhadijeh <feross@feross.org>
2015-01-12 23:20:42 -0800

    downgrade uid correctly

--- server/util.js
+++ server/util.js
@@ -4,7 +4,7 @@ var posix = require('posix')
 var MAX_SOCKETS = 10000
 
 exports.downgradeUid = function () {
-  if (process.platform === 'linux' && process.env === 'production') {
+  if (process.platform === 'linux' && process.env.NODE_ENV === 'production') {
     process.setuid('www-data')
     debug('downgraded uid to ' + process.getuid())
   }
