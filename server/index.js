f5f48fd10bac349106d62e3df290175ab7fe99c4
Feross Aboukhadijeh <feross@feross.org>
2015-03-18 15:13:11 -0700

    Support new RTCIceServer spec
    `RTCIceServer.url` was renamed to `RTCIceServer.urls`

--- server/index.js
+++ server/index.js
@@ -112,9 +112,16 @@ function updateIceServers () {
 
     iceServers = token.ice_servers
       .filter(function (server) {
-        return server && server.url && !/^stun:/.test(server.url)
+        var urls = server.urls || server.url
+        return urls && !/^stun:/.test(urls)
       })
     iceServers.unshift({ url: 'stun:23.21.150.121' })
+
+    // Support new spec (`RTCIceServer.url` was renamed to `RTCIceServer.urls`)
+    iceServers = iceServers.map(function (server) {
+      if (server.urls === undefined) server.urls = server.url
+      return server
+    })
   })
 }
 
