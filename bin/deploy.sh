13290c6719acb897fcee7d75b8423f593b98d39b
Feross Aboukhadijeh <feross@feross.org>
2015-03-27 16:38:50 +1300

    deploy: stop before install

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # Update code and restart server (run from app server)
 set -e
+sudo supervisorctl reload
+sleep 3
+sudo supervisorctl stop instant
 cd /home/feross/www/instant.io && git pull
 cd /home/feross/www/instant.io && rm -rf node_modules
 cd /home/feross/www/instant.io && npm install --quiet
 cd /home/feross/www/instant.io && npm run build
-sudo supervisorctl reload && sleep 3 && sudo supervisorctl restart instant
+sudo supervisorctl start instant
