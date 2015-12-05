959afde4031fe1b7a25d8ef4a886c2f0edbbeb4d
Feross Aboukhadijeh <feross@feross.org>
2015-12-05 03:07:20 -0800

    only one tracker
    For https://github.com/feross/webtorrent/issues/502

--- client/index.js
+++ client/index.js
@@ -12,7 +12,7 @@ var xhr = require('xhr')
 
 var util = require('./util')
 
-global.WEBTORRENT_ANNOUNCE = [ 'wss://tracker.webtorrent.io', 'wss://tracker.btorrent.xyz' ]
+global.WEBTORRENT_ANNOUNCE = [ 'wss://tracker.webtorrent.io' ]
 
 if (!Peer.WEBRTC_SUPPORT) {
   util.error('This browser is unsupported. Please use a browser with WebRTC support.')
