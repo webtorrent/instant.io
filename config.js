32fc038bae61516ad5a3e8ff9559a25cf4b2b4d9
Feross Aboukhadijeh <feross@feross.org>
2017-02-02 16:54:35 -0800

    Use port 4000 in development

--- config.js
+++ config.js
@@ -1,2 +1,13 @@
-exports.isProd = process.env.NODE_ENV === 'production'
-exports.port = 9100
+const isProd = process.env.NODE_ENV === 'production'
+
+/**
+ * Is site running in production?
+ */
+exports.isProd = isProd
+
+/**
+ * Server listening port
+ */
+exports.port = isProd
+  ? 9100
+  : 4000
